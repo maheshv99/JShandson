6f9d8294b56e5b6d1d498fd63a38fc679e4813a8
maheshv99 <109613219+maheshv99@users.noreply.github.com>
2022-09-12 18:47:30 +0530

    v1

---
+++ home-work/first.js
@@ -0,0 +1,31 @@
+//"Problem:
+//You are given a string (STR) of length N, consisting of only the lower case English alphabet.
+//Your task is to remove all the duplicate occurrences of characters in the string.
+
+function show(st){
+    let n=new Set(st);
+    console.log(n);
+}
+
+show("abcadeecfb");
+
+//"Problem:
+//You are given a string (STR) of length N, you have to print the count of all alphabet.(using maps)
+let m2=new Map();
+function mp(st1){
+   let l=st1.length;
+   let s1=new Set(st1);
+   for(let i of s1){
+    let count=0;
+    for(let j of st1){
+        if(i==j){
+            count++;
+        }
+    }
+    m2.set(i,count);
+   }
+}
+mp("abcadeecfb");
+for(let k of m2){
+    console.log(k);
+}
