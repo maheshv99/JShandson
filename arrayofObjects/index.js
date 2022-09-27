3880bed9c069ef87e6fc69e6f551157e135e9534
maheshv99 <109613219+maheshv99@users.noreply.github.com>
2022-09-27 14:09:40 +0530

    v1

---
+++ arrayofObjects/index.js
@@ -0,0 +1,6 @@
+let studentRecords = [ 
+    {name: 'John', id: 123, marks : 98 },
+    {name: 'Baba', id: 101, marks : 23 },  
+    {name: 'yaga', id: 200, marks : 45 },
+    {name: 'Wick', id: 115, marks : 75 } 
+]
